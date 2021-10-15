3f31e73fb3b37c83edce10bc5d9559fdea518089
Chanuk Jeong <jcu531@naver.com>
2021-10-15 23:32:20 +0900

    1. Spring Data Jpa 적용

--- src/main/java/winter/spring/SpringConfig.java
+++ src/main/java/winter/spring/SpringConfig.java
@@ -6,15 +6,19 @@ import org.springframework.context.annotation.Configuration;
 import winter.spring.repository.*;
 import winter.spring.service.MemberService;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
 
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
 //    private DataSource dataSource;
@@ -22,18 +26,18 @@ public class SpringConfig {
 //        this.dataSource = dataSource;
 //    }
 
-    private EntityManager entityManager;
+//    private EntityManager entityManager;
 
-    @Autowired
-    public SpringConfig(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
+//    @Autowired
+//    public SpringConfig(EntityManager entityManager) {
+//        this.entityManager = entityManager;
+//    }
 
-    @Bean
-    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//        return new JdbcMemberRepository(dataSource);
-//        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepositoty(entityManager);
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+////        return new MemoryMemberRepository();
+////        return new JdbcMemberRepository(dataSource);
+////        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepositoty(entityManager);
+//    }
 }
