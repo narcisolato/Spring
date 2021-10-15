792caf16d0d471a7d052563da8247f3f851f0f90
Chanuk Jeong <jcu531@naver.com>
2021-10-15 19:13:40 +0900

    1. jdbc test code 작성

---
+++ src/test/java/winter/spring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,57 @@
+package winter.spring.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import winter.spring.domain.Member;
+import winter.spring.repository.MemberRepository;
+import winter.spring.repository.MemoryMemberRepository;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        // given
+        var member = new Member();
+        member.setName("spring");
+
+        // when
+        var saveId = memberService.join(member);
+
+        // then
+        var findMember = memberService.findOne(saveId).get();
+        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        var member1 = new Member();
+        member1.setName("spring");
+
+        var member2 = new Member();
+        member2.setName("spring");
+
+        memberService.join(member1);
+        var e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        System.out.println(e);
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
